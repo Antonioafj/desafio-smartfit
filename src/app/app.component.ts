3904331fe387109b3186d53b3f2853a5dd817c47
Antonioafj <antonioafj.edu@gmail.com>
2025-03-16 23:22:19 -0400

    feat: add legends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { CardsListComponent } from "./components/cards-list/cards-list.component
 import { CommonModule } from '@angular/common';
 import { Location } from './types/location.interface';
 import { GetUnitsService } from './services/get-units.service';
+import { LegendComponent } from "./components/legend/legend.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FormsComponent, CardsListComponent, CommonModule, LegendComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
